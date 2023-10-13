5b9d0b791923ffa928ad62c64385c77d9d9115fd
AlexIsUnstable <145076032+AlexIsUnstable@users.noreply.github.com>
2023-10-13 18:36:40 -0500

    Update script.js

--- Fan Site/styles/script.js
+++ Fan Site/styles/script.js
@@ -1,1 +1,18 @@
+const displayedImage = document.getElementById('displayedImage');
+const changeImageButton = document.getElementById('changeImageButton');
+
+const imageArray = [
+    'quote1.jpg',
+    'quote2.jpg',
+    'quote3.jpg',
+    'quote4.png'
+];
+
+let currentIndex = 0;
+
+changeImageButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % imageArray.length;
+    const nextImage = imageArray[currentIndex];
+    displayedImage.src = nextImage;
+});
 
